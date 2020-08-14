1ecb5dd3997a4ec665b02d6baaceb290dabda20e
aishwarya-anvekar <aishwaryaanvekar33@gmail.com>
2020-08-14 05:48:06 +0530

    Updates

---
+++ precache-manifest.ee5172846796d7c9cfaee8600ed99761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10cf02d0b47b248f007303582eed0532",
+    "url": "/aishu/index.html"
+  },
+  {
+    "revision": "e43895b1a596e98c01df",
+    "url": "/aishu/static/css/main.90202247.chunk.css"
+  },
+  {
+    "revision": "e1f2c0b0ec95d604cb7a",
+    "url": "/aishu/static/js/2.fa80e68e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aishu/static/js/2.fa80e68e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e43895b1a596e98c01df",
+    "url": "/aishu/static/js/main.8fe954c0.chunk.js"
+  },
+  {
+    "revision": "0ffee0d5e29aa3c7a234",
+    "url": "/aishu/static/js/runtime-main.3c959af9.js"
+  }
+]);
